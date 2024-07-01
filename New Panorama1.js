3cba7ad01fd2c25e33ee47528f71d2890e399973
tradfallning <123250514+tradfallning@users.noreply.github.com>
2024-07-01 13:10:41 +0200

    Add files via upload

---
+++ New Panorama1.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "New Panorama1_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 48.254213415999665,
+	        "minpan": 0,
+	        "mintilt": -32.97961528504927
+	    },
+	"id": "New Panorama1",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"New Panorama1_tiles/New Panorama1_t_1b_%0y_%0x.jpg"},"down":{"src":"New Panorama1_tiles/New Panorama1_t_1d_%0y_%0x.jpg"},"front":{"src":"New Panorama1_tiles/New Panorama1_t_1f_%0y_%0x.jpg"},"height":6041,"left":{"src":"New Panorama1_tiles/New Panorama1_t_1l_%0y_%0x.jpg"},"right":{"src":"New Panorama1_tiles/New Panorama1_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama1_tiles/New Panorama1_t_1u_%0y_%0x.jpg"},"width":6041},{"back":{"src":"New Panorama1_tiles/New Panorama1_t_2b_%0y_%0x.jpg"},"down":{"src":"New Panorama1_tiles/New Panorama1_t_2d_%0y_%0x.jpg"},"front":{"src":"New Panorama1_tiles/New Panorama1_t_2f_%0y_%0x.jpg"},"height":3021,"left":{"src":"New Panorama1_tiles/New Panorama1_t_2l_%0y_%0x.jpg"},"right":{"src":"New Panorama1_tiles/New Panorama1_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama1_tiles/New Panorama1_t_2u_%0y_%0x.jpg"},"width":3021},{"back":{"src":"New Panorama1_tiles/New Panorama1_t_3b_%0y_%0x.jpg"},"down":{"src":"New Panorama1_tiles/New Panorama1_t_3d_%0y_%0x.jpg"},"front":{"src":"New Panorama1_tiles/New Panorama1_t_3f_%0y_%0x.jpg"},"height":1511,"left":{"src":"New Panorama1_tiles/New Panorama1_t_3l_%0y_%0x.jpg"},"right":{"src":"New Panorama1_tiles/New Panorama1_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama1_tiles/New Panorama1_t_3u_%0y_%0x.jpg"},"width":1511},{"back":{"src":"New Panorama1_tiles/New Panorama1_t_4b_%0y_%0x.jpg"},"down":{"src":"New Panorama1_tiles/New Panorama1_t_4d_%0y_%0x.jpg"},"front":{"src":"New Panorama1_tiles/New Panorama1_t_4f_%0y_%0x.jpg"},"height":756,"left":{"src":"New Panorama1_tiles/New Panorama1_t_4l_%0y_%0x.jpg"},"right":{"src":"New Panorama1_tiles/New Panorama1_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama1_tiles/New Panorama1_t_4u_%0y_%0x.jpg"},"width":756},{"back":{"src":"New Panorama1_tiles/New Panorama1_t_5b_%0y_%0x.jpg"},"down":{"src":"New Panorama1_tiles/New Panorama1_t_5d_%0y_%0x.jpg"},"front":{"src":"New Panorama1_tiles/New Panorama1_t_5f_%0y_%0x.jpg"},"height":378,"left":{"src":"New Panorama1_tiles/New Panorama1_t_5l_%0y_%0x.jpg"},"right":{"src":"New Panorama1_tiles/New Panorama1_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama1_tiles/New Panorama1_t_5u_%0y_%0x.jpg"},"width":378}],"multilevel":true,"preview":{"src":"New Panorama1_tiles/New Panorama1_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
